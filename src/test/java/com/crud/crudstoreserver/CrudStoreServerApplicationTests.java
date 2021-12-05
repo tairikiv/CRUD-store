eb671c2b5e9445ab1e31fddf80d6ed6b16bb0381
Tairi Kivaste <tairi.kivaste@gmail.com>
2021-12-05 10:07:54 +0200

    Initalized repository

---
+++ src/test/java/com/crud/crudstoreserver/CrudStoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudstoreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudStoreServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
